5d055995e06f29825e2d0677eb17b2aa7a5f0e51
Scarlett Eller <rq6715@gmail.com>
2022-03-24 16:10:53 -0400

    added simpler methods to rooms and visits

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,7 @@
 class RoomsController < ApplicationController
+
+    def show
+        room = Room.find(params[:id])
+        render json: room
+    end
 end
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,2 +1,29 @@
 class VisitsController < ApplicationController
+
+    def show
+        visit = Visit.find(params[:id])
+        render json: visit
+    end
+
+    def guest_visits
+        guest = Guest.find(session[:id])
+        render json: guest.visits
+    end
+
+    def update
+        visit = Visit.find(params[:id])
+        visit.update(visit_params)
+        render json: visit
+    end
+
+    def destroy
+        visit = Visit.find(params[:id])
+        visit.destroy
+        head :no_content
+    end
+
+    private
+    def visit_params
+        params.permit(:check_in, :check_out)
+    end
 end
