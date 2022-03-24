bd7d765ecfd8873ab82aba1eb5e25eaf822a0160
Alison Melendez <alisonmelendez132@gmail.com>
2022-03-24 16:54:29 -0400

    guest controller methods

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,2 +1,35 @@
 class GuestsController < ApplicationController
+
+    def show 
+        guest = find_guest
+        render json: guest 
+    end
+
+    def show_username 
+        guest = find_guest
+        render json: guest.username
+    end
+
+    #user login 
+    def create 
+        guest = Guest.create(guest_params)
+        session[:guest_id] = guest.id
+        render json: guest, status: :created 
+    end
+
+    def update
+        guest = find_guest
+        guest.update(guest_params)
+        render json: guest
+    end
+
+    private 
+
+    def guest_params
+        params.permit(:name, :gender, :profession, :username, :password_confirmation)
+    end
+
+    def find_guest 
+        Guest.find_by(id: session[:guest_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   resources :visits
   resources :rooms
-  resources :guests
+  resources :guests, only:[:show, :create:, :update]
+
+  get/'', to: "guests#show_username"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
