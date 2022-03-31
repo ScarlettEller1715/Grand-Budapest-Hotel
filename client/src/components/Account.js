813ef5b9c28634d8e039323eb630c1af630ff75c
Alison Melendez <alisonmelendez132@gmail.com>
2022-03-31 11:07:05 -0400

    various changes to the amenities page

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -22,35 +22,28 @@ function Account({ user, setUser }){
 
     return (
         <React.Fragment>
-            <Link to="/">Home</Link>
-            <Nav/>
+                {/* <div className="accountContainer"> */}
+                <Link to="/">Home</Link>
+                <Nav/>
+                <h1 id="accountHeader"> Welcome, {user.name} </h1>
 
-            {/* <div className="portraitContainer">
-            <img className="portrait"src="https://www.yimmaukun.com/wp-content/uploads/2019/06/125_%E7%B6%A0%E8%A1%A3%E5%A5%B3%E5%AD%9060.5x50cm2003%E8%89%B2%E6%A8%99.jpg"/>
-            <img className="frame"src="https://o.remove.bg/downloads/9f4b0dc4-1f34-44df-865f-0e4aa259796a/png-transparent-computer-file-gold-pattern-frame-gold-ornate-frame-template-miscellaneous-frame-golden-frame-removebg-preview.png"/>
-            </div> */}
-
-            <h1 id="accountHeader"> Welcome, {user.name} </h1>
-
-            <div className="overallInfo">
-                <div className="CustomerInfo">
-                    <h3>Guest Information:</h3>
-                    <p>Name: {user.name}</p>
-                    <p>Gender: {user.gender}</p>
-                    <p>Profession: {user.profession}</p>
-                    <p>Username: {user.username}</p>
-                    <button onClick={handleLogoutClick}>Logout</button>
-                </div>
-                <div className="VisitContainer">
-                    <div className="VisitInfo">
-                        <h3 id="visitTitle">Your Upcoming Visit:</h3>
+                <div className="overallInfo">
+                    <div className="CustomerInfo">
+                        <h3>Guest Information:</h3>
+                        <p>Name: {user.name}</p>
+                        <p>Gender: {user.gender}</p>
+                        <p>Profession: {user.profession}</p>
+                        <p>Username: {user.username}</p>
+                        <button onClick={handleLogoutClick}>Logout</button>
                     </div>
-                        {visits}
+                    <div className="VisitContainer">
+                        <div className="VisitInfo">
+                            <h3 id="visitTitle">Your Upcoming Visit:</h3>
+                        </div>
+                            {visits}
+                    </div>
                 </div>
-            </div>
-
-            {/* <button onClick={handleLogoutClick}>Logout</button> */}
-            
+            {/* </div>            */}
         </React.Fragment>
     ); 
 }
--- client/src/components/Amenities.js
+++ client/src/components/Amenities.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 import "../styles/amenities.css";
 
 
@@ -8,32 +9,64 @@ function Amenities () {
 
     return (
         <React.Fragment>
-            <Nav />
-            <h1 id="amenitiesHeader">Our Amenities</h1>
-            <div class="AmenitiesCard">
-                <img src="http://4.bp.blogspot.com/-yyMgdDGVvPw/U19D7wmieZI/AAAAAAAAIfs/GNiyWbSzNi8/s1600/GrandBudapest.jpg"/>
-                <p>The Grand Budapest offers nothing less than the epitome of luxury and service to our guests.</p>
-            </div>
-            <div class="AmenitiesCard">
-                <img src="https://cdn.vox-cdn.com/uploads/chorus_asset/file/2403036/15461985257_6584dff43c_h.0.jpg"/>
-                <h2>The Palm Court</h2>
-            </div>
-            <div class="AmenitiesCard">
-                <img src="https://cdn.80.lv/api/upload/content/14/61b313469442b.jpg"/>
-                <h2>The Arabian Baths</h2>
-            </div>
-            <div class="AmenitiesCard">
-                <img src="https://www.funimag.com/wordpress/wp-content/uploads/TGBH-Funicular-1.jpg" />
-                <h2>The Colonnade Funicular</h2>
-            </div>
-            <div class="AmenitiesCard">
-                <img src="https://apis.xogrp.com/media-api/images/3e345b96-f3ee-11e4-be0a-22000aa61a3e" />
-                <h2>Mendl's Luxury Pastries</h2>
-            </div>
-            <div class="AmenitiesCard">
-                <img src="https://lwlies.com/wp-content/uploads/2021/02/boy-with-apple.jpg" />
-                <h2>The Gustave H. Art Collection</h2>
-            </div>
+                <Nav />
+
+                <h1 id="amenitiesHeader">Our Amenities</h1>
+
+                <div class="AmenitiesCard">
+                    <img src="http://4.bp.blogspot.com/-yyMgdDGVvPw/U19D7wmieZI/AAAAAAAAIfs/GNiyWbSzNi8/s1600/GrandBudapest.jpg"/>
+                    <p>The Grand Budapest offers nothing less than the epitome of luxury and service to our guests.</p>
+                </div>
+
+                <div class="AmenitiesCard">
+                    <img src="https://cdn.vox-cdn.com/uploads/chorus_asset/file/2403036/15461985257_6584dff43c_h.0.jpg"/>
+
+                    <div class="amenitiesInfoCard">
+                        <h2 className="amenitiesSubHeader">The Palm Court</h2>
+                        <p>Stuff about palm court blah blah look at me tehe</p>
+                    </div>
+
+                </div>
+
+                <div class="AmenitiesCard">
+                    <img src="https://cdn.80.lv/api/upload/content/14/61b313469442b.jpg"/>
+                    
+                    <div class="amenitiesInfoCard">
+                        <h2 className="amenitiesSubHeader">The Arabian Baths</h2>
+                        <p>Stuff about palm court blah blah look at me tehe</p>
+                    </div>
+
+                </div>
+
+                <div class="AmenitiesCard">
+                    <img src="https://www.funimag.com/wordpress/wp-content/uploads/TGBH-Funicular-1.jpg" />
+
+                    <div class="amenitiesInfoCard">
+                        <h2 className="amenitiesSubHeader">The Colonnade Funicular</h2>
+                        <p>Stuff about palm court blah blah look at me tehe</p>
+                    </div>
+
+                </div>
+
+                <div class="AmenitiesCard">
+                    <img src="https://apis.xogrp.com/media-api/images/3e345b96-f3ee-11e4-be0a-22000aa61a3e" />
+
+                    <div class="amenitiesInfoCard">
+                        <h2 className="amenitiesSubHeader">Mendl's Luxury Pastries</h2>
+                        <p>Stuff about palm court blah blah look at me tehe</p>
+                    </div>
+                </div>
+
+                <div class="AmenitiesCard">
+                    <img src="https://lwlies.com/wp-content/uploads/2021/02/boy-with-apple.jpg" />
+
+                    <div class="amenitiesInfoCard">
+                        <h2 className="amenitiesSubHeader">The Gustave H. Art Collection</h2>
+                        <p>Stuff about palm court blah blah look at me tehe</p>
+                    </div>
+
+                </div>
+
         </React.Fragment>
         
     )
